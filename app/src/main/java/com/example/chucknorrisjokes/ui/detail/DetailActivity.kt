80bd08ec16016ab710340ff20d78ac77e7c0fd0d
Choirul Muhtadin <choimuhtadin@gmail.com>
2020-06-01 10:39:06 +0700

    try to add transition

--- app/src/main/java/com/example/chucknorrisjokes/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokes/ui/detail/DetailActivity.kt
@@ -1,6 +1,8 @@
 package com.example.chucknorrisjokes.ui.detail
 
 import android.os.Bundle
+import android.transition.TransitionInflater
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.example.chucknorrisjokes.BR
 import com.example.chucknorrisjokes.R
@@ -14,6 +16,7 @@ import kotlinx.android.synthetic.main.lyt_joke_share.view.*
 import kotlinx.android.synthetic.main.lyt_offline.view.*
 
 class DetailActivity : BaseActivity<ActivityDetailBinding>(){
+    private val TAG = "DetailActivity"
 
     lateinit var viewModel: DetailViewModel
 
@@ -22,6 +25,7 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(){
     }
 
     override fun onViewReady(savedInstance: Bundle?) {
+        window.sharedElementEnterTransition = TransitionInflater.from(this).inflateTransition(R.transition.shared_element_transation)
         initViewModel()
 
         val model : ModelJoke = intent.getParcelableExtra("model")
@@ -36,11 +40,17 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(){
         lytOffline.btnRetry.setOnClickListener {
             viewModel.getRandomJoke()
         }
+
+        Log.d(TAG, "onViewReady: ");
     }
 
     private fun initViewModel(){
         viewModel = ViewModelProvider(this,viewModelFactory).get(DetailViewModel::class.java)
         binding.setVariable(BR.viewModel, viewModel)
         binding.lifecycleOwner = this
+    }
+
+    override fun onBackPressed() {
+       finishAfterTransition()
     }
 }
